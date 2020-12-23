1f1bbc11ea55b006a6584df3e941075e02b41679
Llorenç Palliser <palliser@palliser.es>
2020-12-23 13:34:06 +0100

    Corregido error en el botón clean

--- config/routes.js
+++ config/routes.js
@@ -3,8 +3,7 @@ const commonController = require('../controllers/common.controller');
 
 const router = express.Router();
 
-// router.get('/about', aboutController.about);
 router.get('/', commonController.episodes)
-// router.get('/episodes', commonController.episodes)
+router.get('/episodes', commonController.episodes)
 
 module.exports = router;
--- controllers/common.controller.js
+++ controllers/common.controller.js
@@ -1,11 +1,3 @@
-// module.exports.about = (req, res, next) => {
-//     res.render('common/about', {
-//         name: 'Julio',
-//         time: new Date().toString(),
-//         showTime: Math.random() > 0.5,
-//         cities: ['Menorca', 'Huelva', 'Barcelona'],
-//     });
-// };
 
 const epis = require('../data/got.json')
 
