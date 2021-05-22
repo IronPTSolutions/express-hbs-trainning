9b0dc5b36af9ffb748be839b049991e5a5c44d3b
Julio García Pérez <julio.garciaperez@telefonica.com>
2021-05-22 11:29:12 +0200

    feat: episode detail

--- app.js
+++ app.js
@@ -1,13 +1,28 @@
 const express = require('express');
 const app = express();
-
+const data = require('./data/got.json');
 
 /**
  * View setup
  */
-app.set('views', __dirname + 'views');
+app.set('views', __dirname + '/views');
 app.set('view engine', 'hbs');
 
+app.get('/episodes', (req, res) => {
+    res.render('episodes.hbs', { episodes: data });
+});
+
+app.get('/episodes/:id', (req, res) => {
+    const episode = data.find((e) => e.id === Number(req.params.id));
+
+    res.render('episode', {
+        episode: episode,
+    });
+});
+
+app.get('/', (req, res) => {
+    res.redirect('/episodes');
+});
 
 const port = 3000;
 app.listen(port, () => {
